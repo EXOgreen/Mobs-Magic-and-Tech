324bf702c43632c5373a8c908769f5cce8956011
EXOgreen <EXOgreen23@gmail.com>
2014-09-11 14:57:52 -0500

    Back, changed file names.

---
+++ src/main/java/EXOGryph/MMAT/MMAT.java
@@ -0,0 +1,29 @@
+package EXOGryph.MMAT;
+
+/**
+ * Created by Benjamin on 9/11/2014.
+ */
+
+        import cpw.mods.fml.common.Mod;
+        import cpw.mods.fml.common.Mod.EventHandler;
+        import cpw.mods.fml.common.Mod.Instance;
+        import cpw.mods.fml.common.event.FMLInitializationEvent;
+        import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+        import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+        import cpw.mods.fml.common.SidedProxy;
+        import net.minecraft.init.Blocks;
+
+@Mod(modid = MMAT.MODID, version = MMAT.VERSION, name=MMAT.NAME)
+public class MMAT {
+
+    public static final String MODID = "MMAT";
+    public static final String VERSION = "1.0.0";
+    public static final String NAME = "Mobs Magic and Tech";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
+    }
+}
