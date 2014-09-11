28d1f11586c83472374e13d3ffd103d3b94d6f4f
EXOgreen <EXOgreen23@gmail.com>
2014-09-11 14:02:39 -0500

    Just a little change before I go.

--- src/main/java/MMAT/mmat/EXOGryph/MMAT.java
+++ src/main/java/MMAT/mmat/EXOGryph/MMAT.java
@@ -13,11 +13,12 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.SidedProxy;
 import net.minecraft.init.Blocks;
 
-@Mod(modid = MMAT.MODID, version = MMAT.VERSION)
+@Mod(modid = MMAT.MODID, version = MMAT.VERSION, name=MMAT.NAME)
 public class MMAT {
 
-    public static final String MODID = "Magic Mobs and Tech";
+    public static final String MODID = "MMAT";
     public static final String VERSION = "1.0.0";
+    public static final String NAME = "Magic Mobs and Tech";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
